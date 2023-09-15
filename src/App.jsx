f04c5331a2b2abfba059161ec482da0cd6ff32b2
Roknuzzaman5546 <roknuzzamansojib78gmail.com>
2023-09-15 11:57:22 +0600

    toast cade & credit hour code commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react';
 import './App.css'
 import Addlist from './Componants/Addlist/Addlist'
 import Bloogs from './Componants/Bloogs/Bloogs'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [names, setNames] = useState([]);
+  const [credithour, setcreditHour] = useState(0);
+  const [remaininghour, setRemainingHour] = useState(20);
 
-  const handleAddnames = (name) =>{
-        let newNames = [...names, name]
-        setNames(newNames);
+  const handleAddnames = (name, hours) =>{
+    const newName = names.find(listname => listname == name)
+    if(name == newName){
+      return toast('This data is already here') 
+    }
+    let newNames = [...names, name]
+    setNames(newNames);
+    console.log(hours);
+    const setCredit = credithour + hours;
+    setcreditHour(setCredit);
+    
   };
 
   return (
@@ -16,8 +28,12 @@ function App() {
       <h1 className='text-3xl font-bold text-center my-4 mb-5'>Course Registration</h1>
       <div className='flex gap-2 w-11/12 mx-auto'>
         <Bloogs handleAddnames={handleAddnames}></Bloogs>
-        <Addlist names={names}></Addlist>
+        <Addlist 
+        names={names}
+        credithour={credithour}
+        ></Addlist>
       </div>
+      <ToastContainer />
     </>
   )
 }
--- src/Componants/Addlist/Addlist.jsx
+++ src/Componants/Addlist/Addlist.jsx
@@ -1,23 +1,29 @@
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
-const Addlist = ({names}) => {
-    // console.log(names);
-    useEffect(() => {},[newNames])
+const Addlist = ({names, credithour}) => {
+    // console.log(credithour);
+    // useEffect(() => {},[newNames])
     return (
         <div className=" md:w-1/3 "> 
             <div className=" bg-slate-100 p-2 rounded-md">
                 <h3 className=" text-2xl font-bold text-blue-600 border-b-2 pb-1">Credit Hour Remaining 7 hr</h3>
             </div>
             <div>
+                <h3 className='text-center text-3xl text-orange-500 my-2 font-bold'>Course Name</h3>
                 {
-                    names.map(name => newNames.push(name))                
+                    names.map((name, idx) => <h3 
+                    key={idx}
+                    className=' text-2xl font-semibold'
+                    >{name}</h3>)                
                 }
+                <h3>Total credit hour: {credithour}</h3>
             </div>
         </div>
     );
 };
 Addlist.propTypes ={
-    names: PropTypes.object.isRequired
+    names: PropTypes.object.isRequired,
+    credithour: PropTypes.object.isRequired
 }
 export default Addlist;
